02d3cc06112e8a76bb79ff3375619534dbfe974f
melihkurtaran <melihkurtaran@sabanciuniv.edu>
2019-12-19 01:16:26 +0300

    secant method implementation

---
+++ project #1/secantMethod.m
@@ -0,0 +1,20 @@
+clear all; clc; close all;
+format long;
+%Secant Method
+
+f =@(x)x^3 + 2*x^2 + 10*x - 20;
+x(1) = 0; % first bound
+x(2) = 2; % second bound
+
+error = 1000;
+tolerance=1e-10;
+
+iter = 0; %iteration number
+i = 3;
+while (error>tolerance && iter < 200)
+    x(i) = x(i-1) - (f(x(i-1)))*((x(i-1) - x(i-2))/(f(x(i-1)) - f(x(i-2))));
+    iter = iter + 1; %increas iteration number
+    error = abs(x(i) - x(i-1));
+    i = i + 1;
+    root = x(i-1);
+end
